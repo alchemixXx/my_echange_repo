84b22582db4045a896aee1112bcce726521c9613
Pavlo Pavlenko <alskarf@gmail.com>
2019-03-29 07:07:53 +0200

    templates started

--- medical_relief/flaskblog.py
+++ medical_relief/flaskblog.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': "Corney Schafter",
+        'title': "Blog post 1",
+        'content': 'First blog post',
+        'date': 'April 20, 2018',
+    },
+    {
+        'author': "Pavel D",
+        'title': "Blog post 2",
+        'content': 'Second blog post',
+        'date': 'April 21, 2018',
+    },
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "Home page!"
+    return render_template('home.html', post = posts)
 
 
 @app.route("/about")
