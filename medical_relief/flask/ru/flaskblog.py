04f276373315d7acea01f168692f2f72efef78a4
Pavlo Pavlenko <alskarf@gmail.com>
2019-04-10 08:29:51 +0300

    added empty files to each page

--- medical_relief/flask/ru/flaskblog.py
+++ medical_relief/flask/ru/flaskblog.py
@@ -18,35 +18,124 @@ posts = [
     },
 ]
 
+# About - first navbar section
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts = posts)
+    return render_template('01_home.html', posts = posts)
 
 
 @app.route("/about_us")
 def about_us():
-    return render_template('about_us.html', posts = posts)
+    return render_template('11_about_us.html', posts = posts)
 
 
 @app.route("/history")
 def history():
-    return render_template('history.html', posts = posts)
+    return render_template('12_history.html', posts = posts)
+
 
 @app.route("/team")
 def team():
-    return render_template('team.html', posts = posts)
+    return render_template('13_team.html', posts = posts)
+
 
 @app.route("/member_plus")
 def member_plus():
-    return render_template('member_plus.html', posts = posts)
+    return render_template('14_member_plus.html', posts = posts)
+
 
 @app.route("/more_about")
 def more_about():
-    return render_template('more_about.html', posts = posts)
+    return render_template('15_more_about.html', posts = posts)
+
+
+# Programms - second navbar section
+
+@app.route("/direct_one")
+def progr_one():
+    return render_template('21_program1.html', posts = posts)
+
+
+@app.route("/direct_two")
+def progr_two():
+    return render_template('22_program2.html', posts = posts)
+
+
+@app.route("/direct_three")
+def progr_three():
+    return render_template('23_program3.html', posts = posts)
+
+
+@app.route("/direct_four")
+def progr_frou():
+    return render_template('24_program4.html', posts = posts)
+
+
+@app.route("/directs_all")
+def progrs_all():
+    return render_template('25_programs.html', posts = posts)
+
+
+# Treatment - third navbar section
+
+@app.route("/treatment_one")
+def treat_one():
+    return render_template('31_treat1.html', posts = posts)
+
+
+@app.route("/treatment_two")
+def treat_two():
+    return render_template('32_treat2.html', posts = posts)
+
+
+@app.route("/treatment_three")
+def treat_three():
+    return render_template('33_treat3.html', posts = posts)
+
+
+@app.route("/treatment_four")
+def treat_four():
+    return render_template('34_treat4.html', posts = posts)
+
+
+@app.route("/treatments_all")
+def treats_all():
+    return render_template('35_treats_all.html', posts = posts)
+
+
+# Partners - fourth navbar section
+
+@app.route("/hospitals")
+def hospitals():
+    return render_template('41_partner1.html', posts = posts)
+
+
+@app.route("/doctors")
+def doctors():
+    return render_template('42_partner2.html', posts = posts)
+
+
+@app.route("/partners_all")
+def partns_all():
+    return render_template('45_partners_all.html', posts = posts)
+
+
+# News - fifth navbar section
+
+@app.route("/news")
+def news():
+    return render_template('51_news.html', posts = posts)
+
+
+# Contacts - sixth navbar section
+
+@app.route("/contacts")
+def contacts():
+    return render_template('61_contacts.html', posts = posts)
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
